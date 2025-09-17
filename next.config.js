bd3212c8bbd7a77afc6a534930d7b02bf869b0f1
Lu Ke <lu@imac.lan>
2025-09-17 09:12:14 -0700

    Revert ESLint config changes - build works locally

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: false,
+  },
+}
 
 module.exports = nextConfig
 
